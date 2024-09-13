f14c6364343fa120e7a2640ee1a135ae172e758d
Thilo Wejmelka <petrawej@gmail.com>
2024-09-13 21:07:24 +0200

    Created "games"-page && footer && navbar animations (dissapears on scrolldown)

---
+++ script.js
@@ -0,0 +1,13 @@
+let lastScrollTop = 0;
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top = "-110px"; 
+    } else {
+        navbar.style.top = "0"; 
+    }
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; 
+});
